0187af1229948a359509a8a1fee26d47025b21bd
Volodymyr <maestrov13@gmail.com>
2022-10-02 15:31:51 +0200

    implement flavor isCompatibleWith

--- app/src/main/java/onor/food/domain/flavor/FoodFlavor.java
+++ app/src/main/java/onor/food/domain/flavor/FoodFlavor.java
@@ -15,67 +15,77 @@ public enum FoodFlavor implements Flavor {
     BERRY("berry") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.FRUITY;
+            return flavor == FRUITY;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == FLORAL || flavor == SPICE;
         }
     },
     CITRUS("citrus") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.FRUITY;
+            return flavor == FRUITY;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ALCOHOL || flavor == MEDICINAL
+                    || flavor == MELON || flavor == SPICE
+                    || flavor == SULPHUR || flavor == TREE_FRUIT
+                    || flavor == WOOD || flavor == NUTTY;
         }
     },
     DRIED_FRUIT("dried fruit") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.FRUITY;
+            return flavor == FRUITY;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == MEATY || flavor == PHENOL;
         }
     },
     MELON("melon") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.FRUITY;
+            return flavor == FRUITY;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CITRUS || flavor == FRUIT_LIKE
+                    || flavor == GREEN || flavor == SULPHUR
+                    || flavor == SAVORY;
         }
     },
     TREE_FRUIT("tree fruit") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.FRUITY;
+            return flavor == FRUITY;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CITRUS || flavor == DAIRY
+                    || flavor == FLORAL || flavor == FRUIT_LIKE
+                    || flavor == PETROL || flavor == ROASTED
+                    || flavor == SAVORY;
         }
     },
     TROPICAL("tropical") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.FRUITY;
+            return flavor == FRUITY;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == DAIRY || flavor == FLORAL
+                    || flavor == SAVORY || flavor == SPICE
+                    || flavor == WOOD;
         }
     },
     PHENOL("phenol") {
@@ -86,7 +96,7 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CHOCOLATE || flavor == DRIED_FRUIT || flavor == SMOKE;
         }
     },
     PUNGENT("pungent") {
@@ -97,7 +107,7 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == FLORAL || flavor == SOUR;
         }
     },
     MAILLARD("maillard") {
@@ -114,67 +124,72 @@ public enum FoodFlavor implements Flavor {
     CARAMEL("caramel") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.MAILLARD;
+            return flavor == MAILLARD;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ALCOHOL || flavor == FRUIT_LIKE || flavor == NUTTY
+                    || flavor == SAVORY || flavor == SPICE;
         }
     },
     CHOCOLATE("chocolate") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.MAILLARD;
+            return flavor == MAILLARD;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ALCOHOL || flavor == DAIRY || flavor == EARTHY || flavor == FRUIT_LIKE
+                    || flavor == MEATY || flavor == NUTTY || flavor == PHENOL;
         }
     },
     MEATY("meaty") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.MAILLARD;
+            return flavor == MAILLARD;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ALCOHOL || flavor == CHOCOLATE || flavor == DRIED_FRUIT || flavor == EARTHY
+                    || flavor == FRUIT_LIKE || flavor == SAVORY || flavor == SMOKE;
         }
     },
     NUTTY("nutty") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.MAILLARD;
+            return flavor == MAILLARD;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CARAMEL || flavor == CHOCOLATE || flavor == FRUIT_LIKE
+                    || flavor == SULPHUR || flavor == WOOD;
         }
     },
     ROASTED("roasted") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.MAILLARD;
+            return flavor == MAILLARD;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == FRUIT_LIKE || flavor == MARINE || flavor == PETROL
+                    || flavor == SULPHUR || flavor == TREE_FRUIT;
         }
     },
     TOASTED("toasted") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.MAILLARD;
+            return flavor == MAILLARD;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == DAIRY || flavor == FRUIT_LIKE;
         }
     },
     TERPENE("terpene") {
@@ -191,56 +206,58 @@ public enum FoodFlavor implements Flavor {
     MEDICINAL("medicinal") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.TERPENE;
+            return flavor == TERPENE;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CITRUS || flavor == GREEN || flavor == HERBACEOUS || flavor == SOUR;
         }
     },
     SPICE("spice") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.TERPENE;
+            return flavor == TERPENE;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == BERRY || flavor == CARAMEL || flavor == CITRUS
+                    || flavor == SMOKE || flavor == TROPICAL || flavor == WOOD;
         }
     },
     SMOKE("smoke") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.TERPENE;
+            return flavor == TERPENE;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == MEATY || flavor == PHENOL || flavor == SOUR || flavor == SPICE;
         }
     },
     PETROL("petrol") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.TERPENE;
+            return flavor == TERPENE;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ROASTED || flavor == TREE_FRUIT;
         }
     },
     WOOD("wood") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.TERPENE;
+            return flavor == TERPENE;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CITRUS || flavor == HERBACEOUS
+                    || flavor == NUTTY || flavor == SPICE || flavor == TROPICAL;
         }
     },
     MARINE("marine") {
@@ -251,7 +268,7 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ROASTED || flavor == SAVORY || flavor == SOUR;
         }
     },
     SOUR("sour") {
@@ -262,7 +279,7 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == MARINE || flavor == MEDICINAL || flavor == PUNGENT || flavor == SMOKE;
         }
     },
     SAVORY("savory") {
@@ -273,7 +290,8 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ALCOHOL || flavor == CARAMEL || flavor == MARINE || flavor == MELON
+                    || flavor == MEATY || flavor == TREE_FRUIT || flavor == TROPICAL;
         }
     },
     VEGETAL("vegetal") {
@@ -290,45 +308,46 @@ public enum FoodFlavor implements Flavor {
     EARTHY("earthy") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return false;
+            return flavor == VEGETAL;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return flavor == FoodFlavor.VEGETAL;
+            return flavor == CHOCOLATE || flavor == DAIRY || flavor == FLORAL || flavor == MEATY;
         }
     },
     FRUIT_LIKE("fruit like") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.VEGETAL;
+            return flavor == VEGETAL;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CARAMEL || flavor == CHOCOLATE || flavor == MEATY || flavor == MELON
+                    || flavor == NUTTY || flavor == ROASTED || flavor == TOASTED || flavor == TREE_FRUIT;
         }
     },
     GREEN("green") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.VEGETAL;
+            return flavor == VEGETAL;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == MEDICINAL || flavor == MELON;
         }
     },
     HERBACEOUS("herbaceous") {
         @Override
         public boolean isSubFlavorOf(Flavor flavor) {
-            return flavor == FoodFlavor.VEGETAL;
+            return flavor == VEGETAL;
         }
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == GREEN || flavor == MEDICINAL || flavor == WOOD;
         }
     },
     ALCOHOL("alcohol") {
@@ -339,7 +358,8 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CARAMEL || flavor == CHOCOLATE || flavor == CITRUS
+                    || flavor == DAIRY || flavor == MEATY || flavor == SAVORY;
         }
     },
     SULPHUR("sulphur") {
@@ -350,7 +370,7 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == CITRUS || flavor == MELON || flavor == NUTTY || flavor == ROASTED;
         }
     },
     DAIRY("dairy") {
@@ -361,7 +381,8 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == ALCOHOL || flavor == CHOCOLATE || flavor == EARTHY || flavor == FLORAL
+                    || flavor == TREE_FRUIT || flavor == TROPICAL || flavor == TOASTED;
         }
     },
     FLORAL("floral") {
@@ -372,7 +393,8 @@ public enum FoodFlavor implements Flavor {
 
         @Override
         public boolean isCompatibleWith(Flavor flavor) {
-            return false;
+            return flavor == BERRY || flavor == DAIRY || flavor == EARTHY || flavor == PUNGENT
+                    || flavor == TREE_FRUIT || flavor == TROPICAL;
         }
     };
 
